4bbe6691cb4d8682f38e978ffc95326751b7fcb6
Austin Burns <burnsaustin145@yahoo.com>
2019-03-05 01:00:16 -0500

    added scraping functionality

--- scraping.py
+++ scraping.py
@@ -1,13 +1,57 @@
 from selenium import webdriver
+import pprint
 
 
-class SwdMain:
+class Scrape:
 
     def __init__(self):
-        self.driver_location = "/Users/burnsaustin145/PycharmProjects/SWD_2/venv/bin/chromedriver"
+        self.driver_location = "/Users/burnsaustin145/python_projects/analogy_machine/lib/chromedriver"
         self.driver = webdriver.Chrome(self.driver_location)
-        self.driver.get("https://users.wix.com/signin")
-        self.user = self.driver.find_element_by_name('email')
-        self.password = self.driver.find_element_by_name('password')
-        self.login = self.driver.find_element_by_name('submit')
+        self.driver.get("https://en.wikipedia.org/wiki/Main_Page")
+        self.driver.implicitly_wait(10)
+        self.article_dict = {}
         self.quit_flag = True
+
+    def random_get(self):
+        """gets random articles"""
+        self.driver.get("https://en.wikipedia.org/wiki/Main_Page")
+        self.driver.implicitly_wait(10)
+        random_article = self.driver.find_element_by_css_selector('#n-randompage > a')
+        random_article.click()
+        self.driver.implicitly_wait(10)
+        curr_article = self.driver.current_url
+
+        return curr_article
+
+    def random_scrape(self):
+        """composes random_get"""
+
+        curr_article = self.random_get()
+        print("curr_article = " + curr_article)
+        curr_element = self.driver.find_element_by_class_name("mw-parser-output")
+        self.driver.implicitly_wait(10)
+        print(curr_element)
+        text = curr_element.text
+        print(curr_article, text)
+        return text
+
+    def random_composition(self, n):
+        """iterates random scrape 'n' times and saves
+        results to dict"""
+
+        for foo in range(n):
+            self.article_dict[foo] = self.random_scrape()
+
+    def get_article_dict(self):
+        pprint.pprint(self.article_dict)
+
+
+scrape = Scrape()
+scrape.random_composition(50)
+scrape.get_article_dict()
+
+
+
+
+
+
